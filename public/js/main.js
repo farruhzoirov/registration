ca182185798e42ba5ecc8dc634f4e73565d1a570
sultonbayevogabek <sultonbayevogabek@gmail.com>
2023-10-25 17:04:25 +0500

    initial webpack

---
+++ public/js/main.js
@@ -0,0 +1,25 @@
+export const Modal = () => {
+    const elSecondItem  = document.querySelector(".registration-tabs__item:nth-child(2)");
+    const elThirdItem  = document.querySelector(".registration-tabs__item:nth-child(3)");
+    const elModalContainer = document.querySelector(".modal-container");
+    const elModalButton  = document.querySelector(".modal__button");
+
+    if (elSecondItem) {
+        elSecondItem.addEventListener('click',function () {
+            elModalContainer.classList.add("modal-container--open")
+        })
+    }
+
+    if (elThirdItem) {
+        elThirdItem.addEventListener('click',function () {
+            elModalContainer.classList.add("modal-container--open")
+        })
+    }
+
+    if (elModalButton) {
+        elModalButton.addEventListener('click',function () {
+            elModalContainer.classList.remove("modal-container--open")
+        })
+    }
+}
+
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+    entry: './public/js/index.js',
+    output: {
+        filename: 'scripts.js',
+        path: path.resolve(__dirname, 'public', 'js'),
+    },
+};
