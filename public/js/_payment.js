04ce15d3e3f95d3621a483280ccdc01019d17542
sultonbayevogabek <sultonbayevogabek@gmail.com>
2023-10-25 20:26:38 +0500

    commit

--- public/js/_payment.js
+++ public/js/_payment.js
@@ -2,8 +2,19 @@ import redirect from './_redirect';
 
 export default () => {
     try {
-        
+        const userNameElement = document.getElementById('userName');
+        const userLetterElement = document.getElementById('userLetter');
+        const userPhoneElement = document.getElementById('userPhone');
+        const userTariffElement = document.getElementById('userTariff');
+        const userTariffPriceElement = document.getElementById('userTariffPrice');
+        const { name, phone, tariff } = JSON.parse(localStorage.getItem('user'));
+
+        userNameElement.textContent = name;
+        userLetterElement.textContent = name?.charAt(0);
+        userPhoneElement.textContent = `+${phone?.slice(0, 3)} ${phone?.slice(3, 5)} ${phone?.slice(5, 8)} ${phone?.slice(8, 10)} ${phone?.slice(10, 12)}`
+        userTariffElement.textContent = `${ tariff === '1' ? 'STANDARD' : tariff === '2' ? 'PREMIUM' : 'VIP' }`
+        userTariffPriceElement.textContent = `${ tariff === '1' ? '897 000' : tariff === '2' ? '1 297 000' : ' 1 497 000' }`
     } catch (e) {
-        
+        redirect('')
     }
 }
